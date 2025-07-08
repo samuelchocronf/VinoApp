9f7f9c4401a877820e8cc9dc6d2fd0fff7b86ef8
samuelchocronf <samuelchocronf@gmail.com>
2025-07-08 09:19:28 -0400

    Corregida la importación de CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- ¡Asegúrate de que esta línea esté aquí!
+import './index.css' // <-- Esta línea es crucial
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
